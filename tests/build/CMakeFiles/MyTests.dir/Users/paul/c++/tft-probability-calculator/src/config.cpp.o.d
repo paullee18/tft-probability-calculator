64918648f90d681216731ec0df18d07bb0a6c617
Paul Lee <paulfrancislee.1@gmail.com>
2024-06-09 20:37:34 +0800

    refactor

--- tests/build/CMakeFiles/MyTests.dir/Users/paul/c++/tft-probability-calculator/src/config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyTests.dir/Users/paul/c++/tft-probability-calculator/src/config.cpp.o: \
-  /Users/paul/c++/tft-probability-calculator/src/config.cpp \
-  /Users/paul/c++/tft-probability-calculator/src/../include/config.h
